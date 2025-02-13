u-boot/u-boot

commit cd295286c786ba731190dcf5399f29e0a9189955

usb: cdns3: avoid error messages if phys don't exist
The phys property is optional so don't complain
if it doesn't exist in device tree.

Signed-off-by: Roger Quadros <rogerq@kernel.org>

drivers/usb/cdns3/core.c
@@ -333,20 +333,28 @@ static int cdns3_probe(struct cdns3 *cdns)
 	mutex_init(&cdns->mutex);
 
 	ret = generic_phy_get_by_name(dev, "cdns3,usb2-phy", &cdns->usb2_phy);
-	if (ret)
-		dev_warn(dev, "Unable to get USB2 phy (ret %d)\n", ret);
-
-	ret = generic_phy_init(&cdns->usb2_phy);
-	if (ret)
+	if (!ret) {
+		ret = generic_phy_init(&cdns->usb2_phy);
+		if (ret) {
+			dev_err(dev, "USB2 PHY init failed: %d\n", ret);
+			return ret;
+		}
+	} else if (ret != -ENOENT && ret != -ENODATA) {
+		dev_err(dev, "Couldn't get USB2 PHY:  %d\n", ret);
 		return ret;
+	}
 
 	ret = generic_phy_get_by_name(dev, "cdns3,usb3-phy", &cdns->usb3_phy);
-	if (ret)
-		dev_warn(dev, "Unable to get USB3 phy (ret %d)\n", ret);
-
-	ret = generic_phy_init(&cdns->usb3_phy);
-	if (ret)
+	if (!ret) {
+		ret = generic_phy_init(&cdns->usb3_phy);
+		if (ret) {
+			dev_err(dev, "USB3 PHY init failed: %d\n", ret);
+			return ret;
+		}
+	} else if (ret != -ENOENT && ret != -ENODATA) {
+		dev_err(dev, "Couldn't get USB3 PHY:  %d\n", ret);
 		return ret;
+	}
 
 	ret = generic_phy_power_on(&cdns->usb2_phy);
 	if (ret)

MontaVista-OpenSourceTechnology/ti-u-boot

commit 545440aaf980b7e76f02a262d25ea74e4fbbd663

TI: HACK: usb: cdns3: continue probe even when USB PHY device does not exist
Prior to commit cd295286c786 ("usb: cdns3: avoid error messages if phys
don't exist"), cdns3_probe() errors out only on failing to initialize the
USB2/USB3 PHY. However, since commit cd295286c786, absence of the PHY's
device driver is also treated as an error, resulting in a regression.

Extend commit cd295286c786 to treat -ENODEV as an acceptable return value
of generic_phy_get_by_name() and continue cdns3_probe() as was the case
prior to the commit.

ENODEV is acceptable in the special case where the previous stage of the
boot process has already configured the USB PHY, due to which it can be
used without the need for re-configuring it. This however is a HACK since
the correct approach is that of probing the driver for the USB PHY and
figuring out in the driver that the PHY has already been configured.

Fixes: cd295286c786 ("usb: cdns3: avoid error messages if phys don't exist")
Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
Reviewed-by: MD Danish Anwar <danishanwar@ti.com>

drivers/usb/cdns3/core.c
@@ -339,7 +339,7 @@ static int cdns3_probe(struct cdns3 *cdns)
 			dev_err(dev, "USB2 PHY init failed: %d\n", ret);
 			return ret;
 		}
-	} else if (ret != -ENOENT && ret != -ENODATA) {
+	} else if (ret != -ENOENT && ret != -ENODATA && ret != -ENODEV) {
 		dev_err(dev, "Couldn't get USB2 PHY:  %d\n", ret);
 		return ret;
 	}
@@ -351,7 +351,7 @@ static int cdns3_probe(struct cdns3 *cdns)
 			dev_err(dev, "USB3 PHY init failed: %d\n", ret);
 			return ret;
 		}
-	} else if (ret != -ENOENT && ret != -ENODATA) {
+	} else if (ret != -ENOENT && ret != -ENODATA && ret != -ENODEV) {
 		dev_err(dev, "Couldn't get USB3 PHY:  %d\n", ret);
 		return ret;
 	}