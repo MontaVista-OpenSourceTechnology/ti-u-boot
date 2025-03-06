MontaVista-OpenSourceTechnology/ti-u-boot

commit d8e316e2655513119d0d2a07a757617875cee6fa

PENDING: spi: cadence-qspi: Add device tree property to specify PHY support
If the device supports PHY tuning, we don't need to run
spi_calibration(). The PHY tuning algorithm will take care of that.

We pull the read delay for non-PHY reads in that case from the device
tree since those two are not always the same.

Signed-off-by: Pratyush Yadav <p.yadav@ti.com>
Signed-off-by: Apurva Nandan <a-nandan@ti.com>
Signed-off-by: Prasanth Babu Mantena <p-mantena@ti.com>
Signed-off-by: Anurag Dutta <a-dutta@ti.com>

drivers/spi/cadence_qspi.c
@@ -208,6 +208,7 @@ static int cadence_spi_probe(struct udevice *bus)
 	priv->fifo_width	= plat->fifo_width;
 	priv->trigger_address	= plat->trigger_address;
 	priv->read_delay	= plat->read_delay;
+	priv->has_phy		= plat->has_phy;
 	priv->ahbsize		= plat->ahbsize;
 	priv->max_hz		= plat->max_hz;
 
@@ -424,6 +425,7 @@ static int cadence_spi_of_to_plat(struct udevice *bus)
 	 */
 	plat->read_delay = ofnode_read_s32_default(subnode, "cdns,read-delay",
 						   -1);
+	plat->has_phy = ofnode_read_bool(subnode, "cdns,phy-mode");
 
 	debug("%s: regbase=%p ahbbase=%p max-frequency=%d page-size=%d\n",
 	      __func__, plat->regbase, plat->ahbbase, plat->max_hz,

drivers/spi/cadence_qspi.h
@@ -212,6 +212,7 @@ struct cadence_spi_plat {
 	fdt_addr_t	ahbsize;
 	bool		use_dac_mode;
 	int		read_delay;
+	bool		has_phy;
 
 	/* Flash parameters */
 	u32		page_size;
@@ -243,6 +244,7 @@ struct cadence_spi_priv {
 	unsigned int	previous_hz;
 	u32		wr_delay;
 	int		read_delay;
+	bool		has_phy;
 
 	struct reset_ctl_bulk *resets;
 	u32		page_size;

commit 052eab55b2a44a73bed72b158abadc23a5390a57

PENDING: spi: cadence-qspi: Remove device tree property for phy support
phy support is controlled by Kconfig, having it added with device
tree is not adding any value.
So remove device tree support.

Fixes: d8e316e26555("PENDING: spi: cadence-qspi: Add device tree property to specify PHY support")

Cc: Anurag Dutta <a-dutta@ti.com>
Cc: Santhosh Kumar <s-k6@ti.com>
Signed-off-by: Udit Kumar <u-kumar1@ti.com>
Reviewed-by: Santhosh Kumar K <s-k6@ti.com>

drivers/spi/cadence_qspi.c
@@ -1396,7 +1396,6 @@ static int cadence_spi_probe(struct udevice *bus)
 	priv->trigger_address	= plat->trigger_address;
 	priv->read_delay	= plat->read_delay;
 	priv->phase_detect_selector = plat->phase_detect_selector;
-	priv->has_phy		= plat->has_phy;
 	priv->phy_pattern_start = plat->phy_pattern_start;
 	priv->phy_tx_start	= plat->phy_tx_start;
 	priv->phy_tx_end	= plat->phy_tx_end;
@@ -1575,7 +1574,7 @@ static void cadence_spi_mem_do_calibration(struct spi_slave *spi,
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
 	int ret;
 
-	if (!IS_ENABLED(CONFIG_CADENCE_QSPI_PHY) || !priv->has_phy)
+	if (!IS_ENABLED(CONFIG_CADENCE_QSPI_PHY))
 		return;
 
 	priv->phy_read_op = *op;
@@ -1706,7 +1705,6 @@ static int cadence_spi_of_to_plat(struct udevice *bus)
 	 */
 	plat->read_delay = ofnode_read_s32_default(subnode, "cdns,read-delay",
 						   -1);
-	plat->has_phy = ofnode_read_bool(subnode, "cdns,phy-mode");
 
 	plat->phy_tx_start = ofnode_read_u32_default(subnode,
 						     "cdns,phy-tx-start",

drivers/spi/cadence_qspi.h
@@ -275,7 +275,6 @@ struct cadence_spi_plat {
 	fdt_addr_t	ahbsize;
 	bool		use_dac_mode;
 	int		read_delay;
-	bool		has_phy;
 	u32		phy_pattern_start;
 	u32		phy_tx_start;
 	u32		phy_tx_end;
@@ -314,7 +313,6 @@ struct cadence_spi_priv {
 	bool		use_dqs;
 	u32		wr_delay;
 	int		read_delay;
-	bool		has_phy;
 	u32		phy_pattern_start;
 	struct spi_mem_op phy_read_op;
 	u32		phy_tx_start;