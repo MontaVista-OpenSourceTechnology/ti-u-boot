c436c73bb7c960e6b296f46eaf79f7209fd42004
Vignesh Raghavendra <vigneshr@ti.com>
2026-02-27 08:37:31 +0530

    PENDING: misc: k3_fuse: Add support to program additional OTP banks
    This adds support for reading and updating user programmable OTP
    on AM62Lx SoCs.
    
    Bank 0 is user OTP bits
    Bank 0xFF is bootmode eFuses
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Anshul Dalal <anshuld@ti.com>

--- drivers/misc/k3_fuse.c
+++ drivers/misc/k3_fuse.c
@@ -24,8 +24,8 @@ int fuse_read(u32 bank, u32 word, u32 *val)
 	}
 
 	/* Make SiP SMC call and send the word in the parameter register */
-	arm_smccc_smc(K3_SIP_OTP_READ, word,
-		      0, 0, 0, 0, 0, 0, &res);
+	arm_smccc_smc(K3_SIP_OTP_READ, bank, word,
+		      0, 0, 0, 0, 0, &res);
 
 	*val = res.a1;
 	if (res.a0 != 0)
@@ -43,8 +43,9 @@ int fuse_prog(u32 bank, u32 word, u32 val)
 {
 	struct arm_smccc_res res;
 
-	if (bank != 0U) {
-		printf("Invalid bank argument, ONLY bank 0 is supported\n");
+	/* Bank 0 is user OTP, Bank 0xFF is bootmode OTP */
+	if (bank && bank != 0xff) {
+		printf("Invalid bank argument\n");
 		return -EINVAL;
 	}
 
